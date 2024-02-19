21b44886172dac50fa096b5fd78a99399ab02aa0
Ainur <sibrinat616@gmail.com>
2024-02-19 09:06:17 +0300

    git init

---
+++ Desktop/projects/SwiftUI/SwiftUIDocsLearning/SwiftUIDocsLearning/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIDocsLearning
+//
+//  Created by Ainur on 19.02.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
